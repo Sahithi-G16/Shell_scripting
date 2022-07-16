113c85ab7428e47ba8c19bb5e2f4355131bbda36
Sahithi-G16 <109416127+Sahithi-G16@users.noreply.github.com>
2022-07-16 16:56:18 +0530

    Add files via upload

---
+++ delete_empty_lines.sh
@@ -0,0 +1,24 @@
+<<Documentation
+Name:G Sahithi
+Date:19/02/2022
+Description:to delete empty lines from a file
+sample input:
+hello
+
+how
+sample output:
+hello
+how
+Documentation
+
+#!/bin/bash/
+if [ $# -eq 1 ] #check if cla's are passed or not
+then
+    echo Empty lines are deleted
+
+    sed -i '/^[[:blank:]]*$/d' $1 #command to del empty lines(start(^) to end($))
+ 
+    cat $1 #print the content of file
+else
+    echo Error: Please pass the file name through command line. #error stmnt to be printed if no clas are passed
+fi
